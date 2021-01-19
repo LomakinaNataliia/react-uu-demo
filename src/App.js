17e162882e89020044bc38057bc6db2ac5ffaede
LomakinaNataliia <lomakinataliia@gmail.com>
2021-01-19 18:53:24 +0200

    Added the GitHub repo Url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <div className="App">
     UU_demo page
+    <footer> This project is{" "}
+      <a href="https://github.com/LomakinaNataliia/react-uu-demo" target="_blank">open-sourced on GitHub </a>
+    </footer>
+    
     </div>
   );
 }
